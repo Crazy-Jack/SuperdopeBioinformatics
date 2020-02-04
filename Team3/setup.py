270feffe23921be7e2f5e9000664818a0668ee91
Tianqin Li <jacklitianqin@gmail.com>
2020-02-04 16:52:26 -0500

    use setuptools to wrap up the pipeline interface from command

---
+++ Team3/setup.py
@@ -0,0 +1,36 @@
+import setuptools
+from distutils.util import convert_path
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+main_ns = {}
+ver_path = convert_path('version.py')
+with open(ver_path) as ver_file:
+    exec(ver_file.read(), main_ns)
+
+setuptools.setup(
+    name="Team3",
+    version=main_ns['__version__'],
+    author="",
+    author_email="",
+    description=" python package ",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Crazy-Jack/SuperdopeBioinformatics",
+    packages=['Team3'],
+    entry_points = {
+        'console_scripts':[
+            #'circDraw = circDraw.__main__:main',
+            'Team3 = Team3.__main__:main',
+            ]
+        },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+    ],
+)
