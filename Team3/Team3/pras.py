39f4e5783d1f04124bb8bda35cae58cee34b3336
Snigdha Agarwal <snigdhaa@br005.pvt.bridges.psc.edu>
2020-02-27 18:37:50 -0500

    PRAS changes

--- Team3/Team3/pras.py
+++ Team3/Team3/pras.py
@@ -4,12 +4,15 @@ import signal
 class PARS:
     def __init__(self, parameters):
         # TODO: fill in as self.xxx = parameters['xxx']
-        pass
+	self.annot_file=parameters['annot_file']
+	self.id_file=parameters['id_file']
+	self.region=parameters['region']
+	self.input_file=parameters['input_file']
 
     def CallPARS(self):
         # TODO: Please follow the try/except paradigm so that user can know which software to look for when it breaks. And also use subprocess to run the command.
         try:
-            command = "" # MODIFY this
+            command = "python3 PRAS_1.0.py -g " + self.annot_file+" -t "+self.id_file+" -m score -s"+self.region+" -i "+self.input_file+" -a pras_assign.txt -w 0 -d 1000" # MODIFY this
             # call the service
             print(command)
             p = subprocess.Popen(
--- install.sh
+++ install.sh
@@ -1,1 +1,15 @@
 pip3 install -e Team3
+
+## PRAS setup commands
+wget https://github.com/ouyang-lab/PRAS/blob/master/zipped_code/script.zip
+unzip script.zip
+wget http://hgdownload.soe.ucsc.edu/admin/exe/linux.x86_64/gtfToGenePred
+chmod +x gtfToGenePred
+futurize -w script/PRAS_1.0.py
+futurize -w script/extract_genomic_regions.py
+futurize -w script/genepred2PRAS.py
+wget https://github.com/arq5x/bedtools2/releases/download/v2.29.2/bedtools.static.binary
+mv bedtools.static.binary bedtools
+chmod a+x bedtools
+PATH="$PATH:$(pwd)"
+cd script
