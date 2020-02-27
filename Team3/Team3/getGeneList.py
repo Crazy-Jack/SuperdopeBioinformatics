269df1a5b5afc8f6386d105020c6d73e274cd945
Miranda Chung <46878559+mirandaricha@users.noreply.github.com>
2020-02-27 17:42:35 -0500

    Add files via upload

--- Team3/Team3/getGeneList.py
+++ Team3/Team3/getGeneList.py
@@ -1,19 +1,15 @@
 """
-getGeneList.py takes in one argument of PRAS result .txt file and outputs a .txt file of all the genes from PRAS result
-
-Runs with command line "python getGeneList.py inputfile.txt"
+getGeneList is a function takes in PRAS result .txt file as input and outputs a list of top 5% ranked genes from PRAS result
 
 Top 5% of the ranked genes are used for GO analysis
 """
 
-import sys
+def getGeneList(filename):
+    with open(filename) as pras:
+        gene_list = [line.split()[0] for line in pras]
+    
+    gene_list = gene_list[1: int(len(gene_list)*0.05)+2]
+    
 
-with open(sys.argv[1]) as pras:
-    gene_list = [line.split()[0] for line in pras]
-
-gene_list = gene_list[1:(len(gene_list)*0.05)+1]
-
-with open("GeneList.txt","w") as f:
-    for gene in gene_list:
-        f.write('%s\n' % gene) 
+    return gene_list
 
