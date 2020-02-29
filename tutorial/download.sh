1c941a043080f771de24dbfc42c8c6e64d6cd10d
KaKiG <zeyuankn@gmail.com>
2020-02-28 21:10:00 -0500

    Quick download shell for example data

---
+++ tutorial/download.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+wget https://encodeproject.org/ENCFF160MKE/@@download/ENCFF160MKE.fastq.gz
+wget https://encodeproject.org/ENCFF343DKZ/@@download/ENCFF343DKZ.fastq.gz
+wget ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_26/gencode.v33.transcripts.fa.gz
+wget ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_26/gencode.v33.annotation.gtf.gz
